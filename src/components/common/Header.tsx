a85e28956ddc402b06b2b49e057accdc164e286d
jiyoon <jylee@crscube.co.kr>
2023-08-08 09:24:07 +0900

    Header 배치 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,16 +1,24 @@
 import { Link as ReactRouterLink } from 'react-router-dom';
-import { Flex, HStack, Link as ChakraLink } from '@chakra-ui/react';
+import {Button, Flex, HStack, Link as ChakraLink, Spacer} from '@chakra-ui/react';
 
 const Header = () => {
   return <header>
-    <Flex w={'100%'} alignItems='center' justify={'space-between'} >
+    <Flex
+			w={'100%'}
+			alignItems={'center'}
+			justify={'space-between'}
+			p={4}
+		>
       <div>Logo</div>
-      <HStack spacing='10px'>
+      <Spacer />
+      <HStack flex={1} spacing='10px'>
         <ChakraLink as={ReactRouterLink as any} to={'/'}>홈</ChakraLink>
         <ChakraLink as={ReactRouterLink as any} to={'/cover-letter'}>자기소개서</ChakraLink>
         <ChakraLink as={ReactRouterLink as any} to={'/new-clipping'}>뉴스 스크랩</ChakraLink>
         <ChakraLink as={ReactRouterLink as any} to={'/my-page'}>마이페이지</ChakraLink>
       </HStack>
+			<Spacer />
+			<Button>로그인</Button>
     </Flex>
   </header>;
 };
