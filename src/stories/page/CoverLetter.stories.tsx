e25bd5c6eb1d96e211fbc5123256af388f4b6d84
jiyoon0605 <ljy10499@gmail.com>
2023-08-09 22:17:31 +0900

    [FEAT] 자소서 페이지 스토리 추가

---
+++ src/stories/page/CoverLetter.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta } from '@storybook/blocks';
+import CoverLetter from '@pages/CoverLetter';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '@/theme';
+import GlobalSetting from '@/theme/GlobalSetting';
+import type { StoryObj } from '@storybook/react';
+
+const coverLetterMeta: Meta<typeof CoverLetter> = {
+  title: 'Page/CoverLetter',
+  component: CoverLetter,
+  decorators: [(story) =>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <GlobalSetting />
+        {story()}
+      </ChakraProvider>
+    </BrowserRouter>,
+  ],
+};
+
+export default coverLetterMeta;
+type Story = StoryObj<CoverLetter>;
+export const Common: Story = {
+  args: {},
+};
